d0112cc9b40987eda66661f1260c016de12f45c3
mironVY <mw.kld.rus@gmail.com>
2022-02-18 17:22:00 +0200

    second it

--- src/main/java/com/mironovvlad/shop/rest/controller/CakeController.java
+++ src/main/java/com/mironovvlad/shop/rest/controller/CakeController.java
@@ -1,16 +1,18 @@
 package com.mironovvlad.shop.rest.controller;
 
-import com.mironovvlad.shop.dto.Cake;
-import com.mironovvlad.shop.dto.Cakes;
+import com.mironovvlad.shop.goods.CakesServiceImpl;
+import com.mironovvlad.shop.rest.dto.Cake;
+import com.mironovvlad.shop.rest.dto.Cakes;
 import com.mironovvlad.shop.exception.CakeNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,43 +22,59 @@ import java.util.List;
 public class CakeController {
 
         private final Cakes cakeList = new Cakes();
+        private static long idDCounter = 0;
+        private final CakesServiceImpl cakesService;
 
-        public CakeController(){
-                Cake cake1 = new Cake();
-                cake1.setName("Praga");
-                cake1.setCalories(new BigDecimal(100));
-                cake1.setWeight(new BigDecimal(100));
-                cake1.setPrice(new BigDecimal(100));
-                cake1.setImage("");
-                cake1.setId(1L);
-
-                Cake cake2 = new Cake();
-                cake2.setName("Napoleon");
-                cake2.setCalories(new BigDecimal(100));
-                cake2.setWeight(new BigDecimal(100));
-                cake2.setPrice(new BigDecimal(100));
-                cake2.setImage("");
-                cake2.setId(2L);
-
+        @Autowired
+        public CakeController(CakesServiceImpl cakesService) {
                 List<Cake> tmp = new ArrayList<Cake>();
-                tmp.add(cake1);
-                tmp.add(cake2);
                 cakeList.setCakeList(tmp);
+                this.cakesService = cakesService;
         }
+
+//        public CakeController(){
+//                Cake cake1 = new Cake();
+//                cake1.setName("Praga");
+//                cake1.setCalories(new BigDecimal(100));
+//                cake1.setWeight(new BigDecimal(100));
+//                cake1.setPrice(new BigDecimal(100));
+//                cake1.setImage("");
+//                cake1.setId(1L);
+//
+//                Cake cake2 = new Cake();
+//                cake2.setName("Napoleon");
+//                cake2.setCalories(new BigDecimal(100));
+//                cake2.setWeight(new BigDecimal(100));
+//                cake2.setPrice(new BigDecimal(100));
+//                cake2.setImage("");
+//                cake2.setId(2L);
+//
+//                List<Cake> tmp = new ArrayList<Cake>();
+//                tmp.add(cake1);
+//                tmp.add(cake2);
+//                cakeList.setCakeList(tmp);
+//        }
 
         @GetMapping(value = "cakes", produces = MediaType.APPLICATION_JSON_VALUE)
         public Cakes cakes(){
 
 
-                return cakeList;
+                return cakesService.getCakes();
         }
 
         @GetMapping(value = "cake/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-        public Cake cake(@PathVariable Long id){
+        public Cake cake(@PathVariable Long id) {
 
                 return cakeList.getCakeList().stream().filter(c -> c.getId().equals(id))
                         .findFirst()
                         .orElseThrow(() -> new CakeNotFoundException("No such cake"));
         }
 
+        @PostMapping(path = "cakes", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity<Cake> createCake(@RequestBody @Valid Cake newCake){
+                newCake.setId(idDCounter);
+                idDCounter++;
+                cakeList.getCakeList().add(newCake);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+        }
 }
--- src/main/java/com/mironovvlad/shop/rest/dto/Cake.java
+++ src/main/java/com/mironovvlad/shop/rest/dto/Cake.java
@@ -1,4 +1,4 @@
-package com.mironovvlad.shop.dto;
+package com.mironovvlad.shop.rest.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
