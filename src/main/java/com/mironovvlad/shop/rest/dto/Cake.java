c2934a89f28ccfbc00835881f489f6398e0da91d
mironVY <mw.kld.rus@gmail.com>
2022-02-18 23:05:00 +0200

    second it

--- src/main/java/com/mironovvlad/shop/rest/dto/Cake.java
+++ src/main/java/com/mironovvlad/shop/rest/dto/Cake.java
@@ -3,16 +3,14 @@ package com.mironovvlad.shop.rest.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import java.math.BigDecimal;
 
-
 @Data
-@Schema(description = "Short info about cake")
+@Schema(description = "Short info about cakes")
 @Validated
 public class Cake {
 
